e74c8803f422e112b345fca5c942d8c6ea9c80ce
ginoconexiont <pgiraultmatz@gmail.com>
2017-01-13 16:11:35 +0100

    [config-service] implement config service

--- computer-service/src/main/java/com/excilys/technight/ComputerServiceApplication.java
+++ computer-service/src/main/java/com/excilys/technight/ComputerServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class ComputerServiceApplication {
 
