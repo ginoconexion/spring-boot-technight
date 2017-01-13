5479e03986dfa257ae3d2eb28ed1c6bfc47ccf26
ginoconexiont <pgiraultmatz@gmail.com>
2017-01-13 18:03:24 +0100

    [application-client] implement application as dum micro-proxy

---
+++ application-client/src/main/java/com/excilys/technight/ApplicationClientApplication.java
@@ -0,0 +1,16 @@
+package com.excilys.technight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApplicationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationClientApplication.class, args);
+	}
+}
