4c909492f10b5edc27670d1ad85471db0a0ad6bd
ginoconexiont <pgiraultmatz@gmail.com>
2017-01-13 16:41:10 +0100

    [computer-service] modify computer-service to use extern configuration

--- computer-service/src/main/java/com/excilys/technight/ComputerServiceApplication.java
+++ computer-service/src/main/java/com/excilys/technight/ComputerServiceApplication.java
@@ -3,8 +3,9 @@ package com.excilys.technight;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableAutoConfiguration
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ComputerServiceApplication {
 
